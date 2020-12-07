ad20fded053282b517de45c776fab85e53e7b123
Kamber <kamberkamberovic98@gmail.com>
2020-12-07 23:31:18 +0100

    random gif app

---
+++ src/components/Random_V2.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import useGif from '../useGif';
+
+const Random = () => {
+    const { gif, fetchGif } = useGif();
+
+    return (
+        <div className="container">
+            <h1>Random Gif</h1>
+            <img width="500" src={gif} alt="Random Gif" />
+            <button onClick={fetchGif}>CLICK FOR NEW</button>
+        </div>
+    )
+}
+
+export default Random;
